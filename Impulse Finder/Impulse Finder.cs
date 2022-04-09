adf7a486389caf0bb51c53522ff78897803c3fc1
Mikhail Berdnikov <m.berdnikov@adguard.com>
2022-04-09 22:44:17 +0300

    initial

---
+++ Impulse Finder/Impulse Finder.cs
@@ -0,0 +1,76 @@
+﻿using System.Linq;
+using cAlgo.API;
+
+namespace cAlgo
+{
+    [Indicator(IsOverlay = true, AutoRescale = true, AccessRights = AccessRights.None)]
+    public class ImpulseFinder : Indicator
+    {
+        [Parameter("DeviationPercent", DefaultValue = 0.05, MinValue = 0.01)]
+        public double DeviationPercent { get; set; }
+
+        [Output("Out", Color = Colors.LightGray, Thickness = 1, PlotType = PlotType.Line)]
+        public IndicatorDataSeries Value { get; set; }
+
+        private bool m_IsUpDirection;
+        private double m_ExtremumPrice;
+        private int m_ExtremumIndex;
+
+        private void MoveExtremum(int index, double price)
+        {
+            Value[m_ExtremumIndex] = double.NaN;
+            SetExtremum(index, price);
+        }
+
+        private void SetExtremum(int index, double price)
+        {
+            m_ExtremumIndex = index;
+            m_ExtremumPrice = price;
+            Value[m_ExtremumIndex] = m_ExtremumPrice;
+            m_IsUpDirection = !m_IsUpDirection;
+        }
+
+        private double DeviationPrice
+        {
+            get
+            {
+                double percentRate = m_IsUpDirection ? -0.01 : 0.01;
+                return m_ExtremumPrice * (1.0 + DeviationPercent * percentRate);
+            }
+        }
+
+        private void UpdateExtremum(int index, double high, double low)
+        {
+            bool useMove = m_IsUpDirection ? high >= m_ExtremumPrice : low <= m_ExtremumPrice;
+            bool useSet = m_IsUpDirection ? low <= DeviationPrice : high >= DeviationPrice;
+
+            if (useMove)
+            {
+                MoveExtremum(index, high);
+                return;
+            }
+
+            if (useSet)
+            {
+                SetExtremum(index, low);
+            }
+        }
+
+        public override void Calculate(int index)
+        {
+            double low = Bars.LowPrices[index];
+            double high = Bars.HighPrices[index];
+            if (m_ExtremumPrice == 0.0)
+            {
+                m_ExtremumPrice = high;
+            }
+
+            if (Bars.ClosePrices.Count < 2)
+            {
+                return;
+            }
+
+            UpdateExtremum(index, high, low);
+        }
+    }
+}
