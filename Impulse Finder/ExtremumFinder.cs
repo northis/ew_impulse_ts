296e1aa4f0db588e5c601261292b69cf78d58092
Mikhail Berdnikov <m.berdnikov@adguard.com>
2022-04-14 00:02:33 +0300

    ExtremumFinder

---
+++ Impulse Finder/ExtremumFinder.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using cAlgo.API;
+
+namespace cAlgo
+{
+    public class ExtremumFinder
+    {
+        private double m_ExtremumPrice;
+        private int m_ExtremumIndex;
+
+        private bool m_IsUpDirection;
+
+        private double DeviationPrice
+        {
+            get
+            {
+                double percentRate = m_IsUpDirection ? -0.01 : 0.01;
+                return m_ExtremumPrice * (1.0 + DeviationPercent * percentRate);
+            }
+        }
+
+        private void MoveExtremum(int index, double price)
+        {
+            Extrema.Remove(m_ExtremumIndex);
+            SetExtremum(index, price);
+        }
+
+        private void SetExtremum(int index, double price)
+        {
+            m_ExtremumIndex = index;
+            m_ExtremumPrice = price;
+            Extrema[m_ExtremumIndex] = m_ExtremumPrice;
+        }
+
+        private double DeviationPercent { get; }
+
+        public SortedDictionary<int, double> Extrema { get; }
+
+        public ExtremumFinder(double deviationPercent)
+        {
+            DeviationPercent = deviationPercent;
+            Extrema = new SortedDictionary<int, double>();
+        }
+
+        public void Calculate(int index, Bars bars)
+        {
+            double low = bars.LowPrices[index];
+            double high = bars.HighPrices[index];
+            if (m_ExtremumPrice == 0.0)
+            {
+                m_ExtremumPrice = high;
+            }
+
+            if (bars.ClosePrices.Count < 2)
+            {
+                return;
+            }
+
+            if (m_IsUpDirection ? high >= m_ExtremumPrice : low <= m_ExtremumPrice)
+            {
+                MoveExtremum(index, m_IsUpDirection ? high : low);
+            }
+            else if (m_IsUpDirection ? low <= DeviationPrice : high >= DeviationPrice)
+            {
+                SetExtremum(index, m_IsUpDirection ? low : high);
+                m_IsUpDirection = !m_IsUpDirection;
+            }
+        }
+    }
+}
---
+++ Impulse Finder/ImpulseFinder.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using cAlgo.API;
+
+namespace cAlgo
+{
+    [Indicator(IsOverlay = true, AutoRescale = true, AccessRights = AccessRights.None)]
+    public class ImpulseFinder : Indicator
+    {
+        [Parameter("DeviationPercent", DefaultValue = 0.05, MinValue = 0.01)]
+        public double DeviationPercent { get; set; }
+        
+        private bool m_IsInSetup;
+        private int m_SetupStartIndex;
+        private int m_SetupEndIndex;
+        private ExtremumFinder m_ExtremumFinder;
+        private const double TRIGGER_LEVEL_RATIO = 0.5;
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            m_ExtremumFinder = new ExtremumFinder(DeviationPercent);
+        }
+
+        private string StartSetupLineChartName
+        {
+            get { return "StartSetupLine" + Bars.OpenTimes.Last(1); }
+        }
+
+        private string EndSetupLineChartName
+        {
+            get { return "EndSetupLine" + Bars.OpenTimes.Last(1); }
+        }
+
+        private string EnterChartName
+        {
+            get { return "Enter" + Bars.OpenTimes.Last(1); }
+        }
+
+        private string StopChartName
+        {
+            get { return "SL" + Bars.OpenTimes.Last(1); }
+        }
+
+        private string ProfitChartName
+        {
+            get { return "TP" + Bars.OpenTimes.Last(1); }
+        }
+
+        private void CheckSetup(int index)
+        {
+            SortedDictionary<int, double> extrema = m_ExtremumFinder.Extrema;
+            int count = extrema.Count;
+            if (count < 3)
+            {
+                return;
+            }
+
+            KeyValuePair<int, double> startItem = extrema.ElementAt(count - 3);
+            KeyValuePair<int, double> endItem = extrema.ElementAt(count - 2);
+            KeyValuePair<int, double> lastItem = extrema.ElementAt(count - 1);
+
+            double startValue = startItem.Value;
+            double endValue = endItem.Value;
+            
+            bool isImpulseUp = endValue > startValue;
+            double low = Bars.LowPrices[index];
+            double high = Bars.HighPrices[index];
+
+            if (!m_IsInSetup)
+            {
+                double lastValue = lastItem.Value;
+                if (lastValue >= Math.Max(startValue, endValue)
+                    || lastValue <= Math.Min(startValue, endValue))
+                {
+                    return; // The setup is no longer valid, TP or SL is already hit.
+                }
+
+                double triggerSize = Math.Abs(endValue - startValue) * TRIGGER_LEVEL_RATIO;
+
+                double triggerLevel;
+                bool gotSetup;
+                if (isImpulseUp)
+                {
+                    triggerLevel = endValue - triggerSize;
+                    gotSetup = low <= triggerLevel && low > startValue;
+                }
+                else
+                {
+                    triggerLevel = startValue + triggerSize;
+                    gotSetup = high >= triggerLevel && high < endValue;
+                }
+
+                if (!gotSetup)
+                {
+                    return;
+                }
+
+                m_SetupStartIndex = startItem.Key;
+                m_SetupEndIndex = endItem.Key;
+                m_IsInSetup = true;
+
+                Chart.DrawTrendLine(StartSetupLineChartName, m_SetupStartIndex, startValue, index, triggerLevel,
+                    Color.Gray);
+                Chart.DrawTrendLine(EndSetupLineChartName, m_SetupEndIndex, endValue, index, triggerLevel,
+                    Color.Gray);
+                Chart.DrawIcon(EnterChartName, ChartIconType.Star, index, triggerLevel, Color.White);
+                return;
+            }
+            
+            // Re-define the setup-related start and end values
+            startValue = extrema[m_SetupStartIndex];
+            endValue = extrema[m_SetupEndIndex];
+            isImpulseUp = endValue > startValue;
+
+            bool isProfitHit = isImpulseUp && high >= endValue || 
+                               !isImpulseUp && low <= endValue;
+            if (isProfitHit)
+            {
+                Chart.DrawIcon(
+                    ProfitChartName, ChartIconType.Star, index, endValue, Color.Green);
+                m_IsInSetup = false;
+            }
+
+            bool isStopHit = isImpulseUp && low <= startValue || 
+                             !isImpulseUp && high >= startValue; //add allowance
+            if (isStopHit)
+            {
+                Chart.DrawIcon(
+                    StopChartName, ChartIconType.Star, index, startValue, Color.Red);
+                m_IsInSetup = false;
+            }
+        }
+
+        public override void Calculate(int index)
+        {
+            m_ExtremumFinder.Calculate(index, Bars);
+            CheckSetup(index);
+        }
+    }
+}
