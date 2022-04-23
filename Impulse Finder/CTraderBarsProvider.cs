45864f1c2138d822a9adac20d43386725ae0ab31
Mikhail Berdnikov <north@live.ru>
2022-04-24 00:58:24 +0300

    barsprov

--- Impulse Finder/CTraderBarsProvider.cs
+++ Impulse Finder/CTraderBarsProvider.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using cAlgo.API;
 using cAlgo.API.Internals;
 
@@ -13,51 +9,86 @@ namespace cAlgo
         private readonly Bars m_Bars;
         private readonly MarketData m_MarketData;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CTraderBarsProvider"/> class.
+        /// </summary>
+        /// <param name="bars">The bars.</param>
+        /// <param name="marketData">The market data.</param>
         public CTraderBarsProvider(Bars bars, MarketData marketData)
         {
             m_Bars = bars;
             m_MarketData = marketData;
         }
 
+        /// <summary>
+        /// Gets the low price of the candle by the <see cref="index" /> specified.
+        /// </summary>
+        /// <param name="index">The index.</param>
         public double GetLowPrice(int index)
         {
-            throw new NotImplementedException();
+            return m_Bars.LowPrices[index];
         }
 
+        /// <summary>
+        /// Gets the high price of the candle by the <see cref="index" /> specified.
+        /// </summary>
+        /// <param name="index">The index.</param>
         public double GetHighPrice(int index)
         {
-            throw new NotImplementedException();
+            return m_Bars.HighPrices[index];
         }
-
+        
+        /// <summary>
+        /// Gets the open time of the candle by the <see cref="index" /> specified
+        /// </summary>
+        /// <param name="index">The index.</param>
         public DateTime GetOpenTime(int index)
         {
-            throw new NotImplementedException();
+            return m_Bars.OpenTimes[index];
         }
 
         /// <summary>
         /// Gets the total count of bars collected.
         /// </summary>
-        public int Count { get; }
+        public int Count => m_Bars.Count;
 
         /// <summary>
         /// Gets the time frame of the current instance.
         /// </summary>
         public TimeFrame TimeFrame => m_Bars.TimeFrame;
 
-
+        /// <summary>
+        /// Gets the bars of the specified time frame.
+        /// </summary>
+        /// <param name="timeFrame">The time frame.</param>
         public IBarsProvider GetBars(TimeFrame timeFrame)
         {
-            throw new NotImplementedException();
+            if (timeFrame == TimeFrame)
+            {
+                // Maybe it's better to return null
+                return this;
+            }
+
+            Bars bars = m_MarketData.GetBars(timeFrame);
+            var res = new CTraderBarsProvider(bars, m_MarketData);
+            return res;
         }
 
+        /// <summary>
+        /// Gets the int index of bar (candle) by datetime.
+        /// </summary>
+        /// <param name="dateTime">The date time.</param>
         public int GetIndexByTime(DateTime dateTime)
         {
-            throw new NotImplementedException();
+            return m_Bars.OpenTimes.GetIndexByTime(dateTime);
         }
 
+        /// <summary>
+        /// Gets the open time for the latest bar available.
+        /// </summary>
         public DateTime GetLastBarOpenTime()
         {
-            throw new NotImplementedException();
+            return m_Bars.LastBar.OpenTime;
         }
     }
 }
