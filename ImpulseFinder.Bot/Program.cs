554f9a034467d4b877aa98b3a1bef90163a66fec
Mikhail Berdnikov <north@live.ru>
2022-05-03 00:36:35 +0300

    +bot

---
+++ ImpulseFinder.Bot/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ImpulseFinder.Bot
+{
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            IHost host = Host.CreateDefaultBuilder(args)
+                .ConfigureServices(services =>
+                {
+                    services.AddHostedService<SignalWorker>();
+                })
+                .Build();
+            await host.RunAsync();
+        }
+    }
+}
---
+++ ImpulseFinder.Bot/SignalWorker.cs
@@ -0,0 +1,27 @@
+﻿namespace ImpulseFinder.Bot
+{
+    internal class SignalWorker: IHostedService, IDisposable
+    {
+        private readonly ILogger<SignalWorker> m_Logger;
+
+        public SignalWorker(ILogger<SignalWorker> logger)
+        {
+            m_Logger = logger;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            m_Logger.LogInformation("StartAsync");
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            m_Logger.LogInformation("StopAsync");
+        }
+
+        public void Dispose()
+        {
+            m_Logger.LogInformation("Disposed");
+        }
+    }
+}
