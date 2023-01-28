89ef944a1f2127352caac3bbf9c260c9cfd02463
Mikhail Berdnikov <north@live.ru>
2023-01-28 20:20:50 +0200

    trial

--- EasyGartleyIndicator/EasyGartleyIndicatorBase.cs
+++ EasyGartleyIndicator/EasyGartleyIndicatorBase.cs
@@ -11,7 +11,42 @@ namespace EasyGartleyIndicator
     [Indicator(IsOverlay = true, AutoRescale = true, AccessRights = AccessRights.None)]
     public class EasyGartleyIndicator : GartleyFinderBaseIndicator
     {
-        private readonly DateTime m_DateTime = new DateTime(2023, 2, 15);
+        private readonly DateTime m_DateTime = new DateTimeOffset(
+            2023, 2, 15, 0, 0, 0, TimeSpan.Zero).UtcDateTime;
+        
+        private TimeSpan TrialRest => m_DateTime - Server.TimeInUtc;
+        private bool IsTrialActive => TrialRest.TotalSeconds > 0;
+
+        private bool m_IsLocked;
+        public override void Calculate(int index)
+        {
+            if (m_IsLocked)
+                return;
+
+            if (IsTrialActive)
+            {
+                base.Calculate(index);
+            }
+            else
+            {
+                CheckTrial();
+            }
+        }
+
+        private bool CheckTrial()
+        {
+            if (IsTrialActive)
+            {
+                Chart.DrawStaticText("Trial", $"{nameof(EasyGartleyIndicator)}: Days until trial expiration {Convert.ToInt32(TrialRest.TotalDays)}", VerticalAlignment.Top,
+                    HorizontalAlignment.Center, Color.Green);
+                return true;
+            }
+
+            Chart.DrawStaticText("Trial", $"{nameof(EasyGartleyIndicator)}: Trial time is expired, contact me on Telegram: @soft_udder", VerticalAlignment.Top,
+                HorizontalAlignment.Center, Color.OrangeRed);
+            m_IsLocked = true;
+            return false;
+        }
 
         protected override void Initialize()
         {
@@ -19,17 +54,10 @@ namespace EasyGartleyIndicator
             {
                 return;
             }
-
-            if (Server.TimeInUtc > m_DateTime)
+            
+            if (CheckTrial())
             {
-                Chart.DrawStaticText("Trial", $"{nameof(EasyGartleyIndicator)}: Trial time is expired, contact me on the Telegram: @soft_udder", VerticalAlignment.Top,
-                    HorizontalAlignment.Center, Color.Red);
-            }
-            else
-            {
-                Chart.DrawStaticText("Trial", $"{nameof(EasyGartleyIndicator)}: Trial is working until {m_DateTime:R}", VerticalAlignment.Top,
-                    HorizontalAlignment.Center, Color.Green);
-               base.Initialize();
+                base.Initialize();
             }
         }
     }
