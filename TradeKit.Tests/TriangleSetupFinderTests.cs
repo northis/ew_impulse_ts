689660fe98b7f2b0c326cd609bdf01abd031dec2
Mikhail Berdnikov <north@live.ru>
2025-07-24 01:37:28 +0300

    triangle test passed

--- TradeKit.Tests/TriangleSetupFinderTests.cs
+++ TradeKit.Tests/TriangleSetupFinderTests.cs
@@ -15,6 +15,10 @@ namespace TradeKit.Tests
     /// </summary>
     internal class TriangleSetupFinderTests
     {
+        private readonly string m_TestDataPath =
+            Path.Combine(Environment.CurrentDirectory, "TestData",
+                "TriangleSetupFinderTests_001.csv");
+        
         /// <summary>
         /// Generates triangle pattern candles using PatternGenerator and tests IsSetup invocation.
         /// </summary>
@@ -22,17 +26,20 @@ namespace TradeKit.Tests
         public void TriangleSetupFinder_IsSetup_Invocation()
         {
             // Arrange: Generate triangle pattern candles
-            var patternGenerator = new PatternGenerator(true);
+            /*var patternGenerator = new PatternGenerator(true);
             (DateTime, DateTime) dates = Helper.GetDateRange(100, TimeFrameHelper.Minute5);
             (DateTime, DateTime) datesImp = Helper.GetDateRange(120, TimeFrameHelper.Minute5);
             ModelPattern? model = patternGenerator.GetPattern(
                 new PatternArgsItem(90, 80, dates.Item1, dates.Item2, TimeFrameHelper.Minute5)
                 { Min = 60, Max = 90}, ElliottModelType.TRIANGLE_CONTRACTING);
-            
+
             PatternArgsItem paramArgs = new PatternArgsItem(
-                40, 90, datesImp.Item1, dates.Item1.Subtract(TimeFrameHelper.TimeFrames[TimeFrameHelper.Minute5.Name].TimeSpan), TimeFrameHelper.Minute5);
+                40, 90,  dates.Item1.Subtract(TimeFrameHelper
+                                            .TimeFrames[TimeFrameHelper.Minute5.Name].TimeSpan * 200),
+               datesImp.Item1,
+                TimeFrameHelper.Minute5);
             ModelPattern modelImp = patternGenerator.GetPattern(
-                paramArgs, ElliottModelType.IMPULSE, true);
+                paramArgs, ElliottModelType.ZIGZAG);
 
             // Convert generated candles to (Candle, DateTime) tuples
             var testCandles = new List<(Candle candle, DateTime openTime)>();
@@ -46,7 +53,7 @@ namespace TradeKit.Tests
             {
                 var candle = new Candle(candleExport.O, candleExport.H, candleExport.L, candleExport.C);
                 testCandles.Add((candle, candleExport.OpenDate));
-            }
+            }*/
 
             // Use TestBarsProvider to add candles
             var barsProvider = new TestBarsProvider(TimeFrameHelper.Minute5);
@@ -60,13 +67,23 @@ namespace TradeKit.Tests
                 triangleFinder.CheckBar(
                     barsProvider.GetOpenTime(barsProvider.Count - 1));
             };
+
+            triangleFinder.OnEnter += (_, args) =>
+            {
+
+            };
             
-            barsProvider.AddCandles(testCandles);
-            //barsProvider.SaveCandles();
-            string chartPath = ChartGenerator.GenerateCandlestickChart(
-                new BarPoint(0, barsProvider),
-                new BarPoint(testCandles.Count - 1, barsProvider),
-                barsProvider);
+            triangleFinder.MarkAsInitialized();
+            barsProvider.LoadCandles(m_TestDataPath);
+            
+            // barsProvider.AddCandles(testCandles);
+            // barsProvider.SaveCandles(barsProvider.GetOpenTime(0),
+            //     barsProvider.GetOpenTime(barsProvider.Count - 1),
+            //     m_TestDataPath);
+            // string chartPath = ChartGenerator.GenerateCandlestickChart(
+            //     new BarPoint(0, barsProvider),
+            //     new BarPoint(barsProvider.Count - 1, barsProvider),
+            //     barsProvider);
 
             // Assert: Just ensure invocation, not result (method is not finished)
             //Assert.Pass("IsSetup was invoked without exceptions. Result: " + (result ?? "null"));
