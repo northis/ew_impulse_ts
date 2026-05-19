fe22ef5a167bc0c50c6216ea6558c21837be7c38
Mikhail Berdnikov <north@live.ru>
2026-05-20 02:43:12 +0300

    ew indicator

---
+++ ElliottIndicator/ElliottIndicator.cs
@@ -0,0 +1,10 @@
+﻿using cAlgo.API;
+using TradeKit.CTrader.Indicators;
+
+namespace ElliottIndicator
+{
+    [Indicator(IsOverlay = true, AutoRescale = true, AccessRights = AccessRights.FullAccess)]
+    public class ElliottIndicator : IterativeImpulseZigzagIndicator
+    {
+    }
+}
